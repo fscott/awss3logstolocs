66a22742af641c941d565c6edd9d9fd603d85cb1
fscott <franklinscott@gmail.com>
2015-10-18 16:37:07 -0700

    First commit.

---
+++ awss3logtime.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2015 Franklin Scott
+# http://www.franklinscott.com/
+
+import csv
+import sys
+import os
+import re
+import subprocess
+import argparse
+
+usage = "usage: /.%(prog)s [options] Given a file with a list of ip addresses or a folder with aws s3 logs, print locations from the free Maxmind geoip databases."
+parser = argparse.ArgumentParser(usage=usage)
+
+parser.add_argument("-i", action="store", default='GeoLite2-City-Blocks-IPv4.csv', help="name of ip addresses csv file (default: %(default)s)")
+parser.add_argument("-l", action="store", default='GeoLite2-City-Locations-en.csv', help="name of city locations csv file (default: %(default)s)")
+parser.add_argument("-f", action="store", default='', help="name of file containing ip addresses")
+parser.add_argument("-o", action="store", default='', help="name of output file")
+parser.add_argument("-s3", action="store", default='', help="name of s3 bucket with logging enabled")
+parser.add_argument("-logpath", action="store", default='./root/', help="name the local path to logs (default: %(default)s)")
+parser.add_argument("--skips3", dest="skips3", action="store_true")
+
+options = parser.parse_args()
+s3bucket = options.s3
+
+def map_your_ips(your_ips):
+    output = []
+    all = len(your_ips)
+    curr = 0
+    for line in your_ips:
+        print 'Processing %s out of %s' % (curr,all)
+        curr += 1
+        for x in ips_to_locs: 
+            if x in line:
+                val = ips_to_locs[x]
+                try:
+                    output.append(line.strip() + ' ' + locs_to_places[val])
+                except KeyError:
+                    output.append("No key for " + x)
+    return output
+
+def get_ip_from_s3_log(line):
+    p = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
+    return p.group()
+
+def get_ips_from_logs(path_to_logs):    
+    for filename in os.listdir(path_to_logs):
+        with open(path_to_logs + filename) as f:
+            t = f.readlines()
+            your_ips.append(get_ip_from_s3_log(t[0]))  
+    return your_ips
+
+def open_csv_files():
+    ips = []
+    locs = []          
+    try:
+        with open(options.i) as f:
+            addresses = csv.reader(f)
+            for row in addresses:
+                ips.append(row)
+        
+        with open(options.l) as f2:
+            locations = csv.reader(f2)
+            for row in locations:
+        	    locs.append(row)
+    except IOError:
+        print "Unable to find csv files. Exiting..."
+        sys.exit(1)
+    
+    ips_to_locs = {}
+    for x in ips:
+        trim = x[0].rfind('.')
+        ip = x[0][:trim]
+        ips_to_locs[ip]=x[1]
+    
+    locs_to_places = {}
+    for y in locs:
+        l = [y[5],y[7],y[8],y[9],y[10],y[11],y[12]]
+        for thing in l:
+            if len(thing) < 1:
+                l.remove(thing)
+        place = ' '.join(l)
+        locs_to_places[y[0]] = place  
+    return ips_to_locs, locs_to_places
+
+def write_results(output):
+    if len(options.o) > 1:
+        with open(options.o, 'w') as outfile:
+            for x in output:
+                outfile.write(x)
+        print 'See file %s for output.' % (options.o)
+    else:
+        for x in output:
+            print x
+
+def main():
+    ips_to_locs, locs_to_places = open_csv_files()
+    
+    your_ips = []
+    # We're trying to get the IPs.
+    
+    # If there's an input file with IPs, then check to see whether we have locations for any of them.
+    if len(options.f) > 0:
+        lines = []
+        try:
+            with open(options.f) as f3:
+                your_ips = f3.readlines()
+        except IOError:
+            print 'No file by that name. Exiting'
+            sys.exit(1)
+    
+    # If we're using s3, download the logs and parse them. Or just skip s3 and parse ones we already have.
+    s3success = False
+    if len(s3bucket) > 0 and options.skips3 == False:
+        x = subprocess.call(["aws","s3","cp","s3://logs.%s" % s3bucket, ".", "--recursive"],stderr=subprocess.STDOUT)
+        if x == 0:
+            s3success = True #for test
+    
+    if s3success or skips3:
+        your_ips = get_ips_from_logs(options.logpath)
+                 
+    output = map_your_ips(your_ips)
+    write_results(output)
+
+if __name__ == "__main__":
+    main()
